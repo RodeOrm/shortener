f64c62ca409df98901e0097ff80f329a80afb9aa
a-ilyin <a-ilyin@it-serv.ru>
2024-10-17 21:06:35 +0300

    перемести example в отдельный файл

--- internal/api/apiShorten_test.go
+++ internal/api/apiShorten_test.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"bytes"
-	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -58,22 +57,5 @@ func TestAPIShorten(t *testing.T) {
 			require.NoError(t, err)
 			assert.Equal(t, tt.want.statusCode, result.StatusCode)
 		})
-	}
-}
-
-func ExampleServer_apiShortenHandler() {
-	server := Server{ServerAddress: "http://localhost:8080", Storage: repo.NewStorage("", "")} // С хранилищем в памяти, поэтому мокать  не надо
-	body := `{"url":"http://www.yandex.ru"}`
-	reqURL := "http://localhost:8080/api/shorten"
-
-	request := httptest.NewRequest(http.MethodPost, reqURL, bytes.NewReader([]byte(body)))
-
-	w := httptest.NewRecorder()
-	h := http.HandlerFunc(server.APIShortenHandler)
-	h.ServeHTTP(w, request)
-	result := w.Result()
-	err := result.Body.Close()
-	if err != nil {
-		log.Fatal()
 	}
 }
