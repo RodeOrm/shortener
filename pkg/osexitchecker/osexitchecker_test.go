8482e463689b596568f4a1122e36c0d305219a65
a-ilyin <a-ilyin@it-serv.ru>
2024-11-03 23:09:40 +0300

    документирование

--- pkg/osexitchecker/osexitchecker_test.go
+++ pkg/osexitchecker/osexitchecker_test.go
@@ -6,6 +6,7 @@ import (
 	"golang.org/x/tools/go/analysis/analysistest"
 )
 
+// TestMyAnalyzer тестирует анализатор
 func TestMyAnalyzer(t *testing.T) {
 	// функция analysistest.Run применяет тестируемый анализатор Analyzer
 	// к пакетам из папки testdata и проверяет ожидания
--- pkg/osexitchecker/testdata/pkg1/main.go
+++ pkg/osexitchecker/testdata/pkg1/main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 )
 
+// main - тестовая
 func main() {
 	os.Exit(0) //want "os.Exit is not allowed in main package"
 }
