a051ca9356a40ecfa3654108027c16a226023e82
a-ilyin <a-ilyin@it-serv.ru>
2024-11-25 17:09:01 +0300

    форматирование

--- internal/grpc/client/client.go
+++ internal/grpc/client/client.go
@@ -1,6 +1,6 @@
 // Package client клиентское приложение для взаимодействия через gRPC
 package client
 
-//NewClient создает новый инстанс клиента
+// NewClient создает новый инстанс клиента
 func NewClient() {
 }
